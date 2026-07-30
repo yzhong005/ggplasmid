a733fdd4efa47a3677595f28b3ecc3aed83ca8f8
Zhong Yang <yzhong005@e.ntu.edu.sg>
2026-07-31 01:52:53 +0800

    Add angled linear map demo

--- inst/examples/pSGNDM_5_demo.R
+++ inst/examples/pSGNDM_5_demo.R
@@ -36,13 +36,32 @@ p_linear <- ggplasmid(
   linear_label_max_lines = 2,
   linear_row_spacing = 4.0,
   linear_label_allow_gene_line_crossing = FALSE,
-  label_line_angle = 90,
   label_text_angle = 0,
   label_text_colour = "category",
   label_line_colour = "category",
   legend_position = "right"
 )
 p_linear
+
+# The same four-line map with diagonal connector lines.
+p_linear_45 <- ggplasmid(
+  annotation = gbk_data,
+  fasta = fasta_data,
+  name = "pSGNDM-5",
+  layout = "linear",
+  plot_line_num = 4,
+  max_labels = 36,
+  linear_label_wrap_width = 18,
+  linear_label_max_lines = 2,
+  linear_row_spacing = 4.0,
+  linear_label_allow_gene_line_crossing = FALSE,
+  label_line_angle = 45,
+  label_text_angle = 0,
+  label_text_colour = "category",
+  label_line_colour = "category",
+  legend_position = "right"
+)
+p_linear_45
 
 # Data used by either plot are also available from the plot object.
 used_data <- plasmid_data(p_circular)
